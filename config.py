2f3a8892f966f5886af21523427665f03db1d827
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-07-14 20:55:05 +0800

    update config

--- config.py
+++ config.py
@@ -8,8 +8,10 @@ WHISPER_MODEL = "/home/vellosaurus/whisper.cpp/models/ggml-base.en.bin"
 PIPER_MODEL = "/home/vellosaurus/piper_voices/en_US-amy-medium.onnx"
 
 OLLAMA_URL = "http://localhost:11434/api/chat"
+EMBED_URL = "http://localhost:11434/api/embeddings"
 LLM_MODEL = "qwen2.5:7b-instruct-q4_K_M"
-SYSTEM_PROMPT = "You are a helpful, concise voice assistant."
+EMBED_MODEL = "nomic-embed-text"
+SYSTEM_PROMPT = "You are a voice assistant designed by VelloSaurus. Your job is to become a friend, assistant, and a talking buddy. Your full name is BUFF_NAI, or in short NAI."
 
 LOG_DIR = "logs"
 CONVERSATION_LOG = os.path.join(LOG_DIR, "latest_conversation_log.json")
